b7c3810702cb774afeebba46c69ac9f13e534df1
raf <rnm1816@gmail.com>
2020-06-28 04:42:55 +0300

    installation file

---
+++ install.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Unzip Install
+sudo apt install unzip
+
+# Python 3.8 install
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+
+# PyEcore install
+pip3 install pyecore
+
+# RyEcoreGen install
+pip3 install pyecoregen
+
+# Jinja2 install
+pip3 install -U Jinja2
+
+# TextX install
+pip3 install pyecore
+
+# Matplotlib install
+python -m pip install -U pip
+python -m pip install -U matplotlib
+
+# NetworkX install
+pip3 install networkx
+
+# Weasyprint install
+pip3 install weasyprint
+
+# Download and extract Generos
+wget https://github.com/bronzeRaf/generos/archive/master.zip
+unzip master.zip
